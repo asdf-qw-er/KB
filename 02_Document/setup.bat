4a81847d83d1a4762e50a75176ef026e908a0a24
asdf-qw-er <asdfqwer0113@gmail.com>
2024-11-23 22:24:51 +0900

    # Update STT.py (Author: Lee Dayeon, Cha Ahyeon)

--- 02_Document/setup.bat
+++ 02_Document/setup.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Installing required Python packages...
-pip install opencv-python numpy keras torch SpeechRecognition gtts playsound pyaudio
+pip install opencv-python numpy keras tensorflow torch SpeechRecognition gtts playsound pyaudio librosa pydub noisereduce pipwin
 echo Installation complete.
 pause
